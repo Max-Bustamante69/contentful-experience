f27e085ba0152dd192e6d600b3262f274ebb04eb
Max <mbustamang@eafit.edu.co>
2025-02-19 00:44:57 -0500

    Component card width adjustment

--- src/components/ComponentCard/ComponentCard.tsx
+++ src/components/ComponentCard/ComponentCard.tsx
@@ -12,7 +12,7 @@ export const ComponentCard: React.FC<CardComponentProps> = ({ title, description
     <div
       {...experienceProps}
       id='component-card'
-      className="flex flex-col gap-2 p-4 bg-primary rounded border border-neutral-200 shadow-md items-start"
+      className="flex flex-col gap-2 p-4 bg-primary rounded border border-neutral-200 shadow-md items-start w-full"
     >
       <h2 className="font-bold text-3xl text-black">{title}</h2>
       <p className="text-neutral-500">{description}</p>
