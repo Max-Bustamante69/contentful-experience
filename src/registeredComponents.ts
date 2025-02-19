81f9df3187cced64f388a77856e700c6fc15ed07
Max <mbustamang@eafit.edu.co>
2025-02-19 00:45:03 -0500

    registered all new components

--- src/registeredComponents.ts
+++ src/registeredComponents.ts
@@ -5,6 +5,12 @@ import { Button } from "./components/Button/Button";
 import { buttonDefinition } from "./components/Button/buttonDefinition";
 import { ComponentCard } from "./components/ComponentCard/ComponentCard";
 import { componentCardDefinition } from "./components/ComponentCard/componentCardDefinition";
+import { Tab } from "./components/Tab/Tab";
+import { tabDefinition } from "./components/Tab/tabDefinition";
+import { Toggle } from "./components/Toggle/Toggle";
+import { toggleDefinition } from "./components/Toggle/toggleDefinition";
+import { Select } from "./components/Select/Select";
+import { selectDefinition } from "./components/Select/selectDefinition";
 
 defineComponents([
   {
@@ -14,5 +20,18 @@ defineComponents([
   {
     component: ComponentCard,
     definition: componentCardDefinition
-  }
+  },
+  {
+    component: Tab,
+    definition: tabDefinition
+  },
+  {
+    component: Toggle,
+    definition: toggleDefinition
+  },
+  {
+    component: Select,
+    definition: selectDefinition
+  },
+  
 ]);
