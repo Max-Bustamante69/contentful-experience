b597d9975aa849a771819e33856246d4b054203d
Max-Bustamante69 <124266497+Max-Bustamante69@users.noreply.github.com>
2025-02-18 22:31:08 -0500

    Component display card created

---
+++ src/components/ComponentCard/ComponentCard.tsx
@@ -0,0 +1,22 @@
+// src/components/Button.tsx
+import React from "react";
+
+interface CardComponentProps {
+  title: string;
+  description: string;
+  children: React.ReactNode;
+}
+
+export const ComponentCard: React.FC<CardComponentProps> = ({ title, description, children, ...experienceProps }) => {
+  return (
+    <div
+      {...experienceProps}
+      id='component-card'
+      className="flex flex-col gap-2 p-4 bg-primary rounded border border-neutral-200 shadow-md items-start"
+    >
+      <h2 className="font-bold text-3xl text-black">{title}</h2>
+      <p className="text-neutral-500">{description}</p>
+      {children}
+    </div>
+  );
+};
---
+++ src/components/ComponentCard/componentCardDefinition.ts
@@ -0,0 +1,18 @@
+export const componentCardDefinition = {
+  id: "componentCard",
+  name: "Component Card",
+  category: "Custom Components",
+  children: true,
+  variables: {
+    title: {
+      displayName: "Text",
+      type: "Text",
+      defaultValue: "Component Title",
+    },
+    description: {
+      displayName: "Description",
+      type: "Text",
+      defaultValue: "Component Description",
+    },
+  },
+};
